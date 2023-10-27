9062dd61cca74cf018fc93d4d05d7fd1eb98ecef
Md. Mursalin Hasan Nirob <109570432+mursalin49@users.noreply.github.com>
2023-10-28 01:48:00 +0600

    Add files via upload

---
+++ contact1.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:influxdev/contact/contact3.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    title: 'Flutter Demo',
+    theme: new ThemeData(primarySwatch: Colors.blue),
+    home: new ContactsPage(),
+  ));
+}
